247fc77391010ccaa65e452d552f0fb7573e96e6
OFedorov <offedoroff@yandex.ru>
2014-05-05 16:35:37 +0400

    From Timer to Cycle

--- main.cpp
+++ main.cpp
@@ -2,15 +2,22 @@
 #include <QApplication>
 #include <core.h>
 #include <iostream>
+#include <QThread>
+#include "console.h"
 
 using namespace std;
 int main(int argc, char *argv[])
 {
     cout << "Hello, World" << endl;
     Core a(argc, argv);
-    a.timerStart();
+    a.cycleStart(1);
     MainWindow w;
     w.show();
+//    console cons;
+//    QThread tr;
+//    cons.moveToThread(&tr);
 
+//    tr.start();
+//    cons.run();
     return a.exec();
 }
